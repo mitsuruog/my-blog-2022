9dc9978f94610d25ff7c00545dbd81bb08667508
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2022-06-29 22:38:46 +0900

    WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <div className='text-slate-900'>Test</div>
         </h1>
 
         <p className={styles.description}>
